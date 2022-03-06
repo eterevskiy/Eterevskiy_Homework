3fd1867bd1bd793651405964106ed18d3ee5cf12
eterevskiy <64526979+eterevskiy@users.noreply.github.com>
2022-03-06 16:48:09 +0200

    Add homework 7

--- HW_7/HW_7.py
+++ HW_7/HW_7.py
@@ -1,11 +1,15 @@
 import csv
 import json
+import requests
 
-with open(r"HW_7\todos.json","r") as file :
-    todos = json.load(file)
+# with open(r"HW_7\todos.json","r") as file :
+#     todos = json.load(file)
 
-with open(r"HW_7\users.json","r") as file :
-    users = json.load(file)
+# with open(r"HW_7\users.json","r") as file :
+#     users = json.load(file)
+
+todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+users = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
 for user in users :
     with open(rf"HW_7\users_info\{user['name']}.csv",'w') as file :
