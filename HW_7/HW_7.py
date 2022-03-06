6542e4bad4c0de275588083e2a81d0f328127a1b
eterevskiy <64526979+eterevskiy@users.noreply.github.com>
2022-03-06 16:49:18 +0200

    Update HW_7.py

--- HW_7/HW_7.py
+++ HW_7/HW_7.py
@@ -1,12 +1,5 @@
 import csv
-import json
 import requests
-
-# with open(r"HW_7\todos.json","r") as file :
-#     todos = json.load(file)
-
-# with open(r"HW_7\users.json","r") as file :
-#     users = json.load(file)
 
 todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 users = requests.get('https://jsonplaceholder.typicode.com/users').json()
