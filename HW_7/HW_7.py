1ffe15166310ebb80b6539c9da6bd00d54b98981
eterevskiy <64526979+eterevskiy@users.noreply.github.com>
2022-03-06 15:50:06 +0200

    Add homework 7

---
+++ HW_7/HW_7.py
@@ -0,0 +1,21 @@
+import csv
+import json
+
+with open(r"HW_7\todos.json","r") as file :
+    todos = json.load(file)
+
+with open(r"HW_7\users.json","r") as file :
+    users = json.load(file)
+
+for user in users :
+    with open(rf"HW_7\users_info\{user['name']}.csv",'w') as file :
+        writer = csv.writer(file, delimiter = ",")
+        writer.writerow(["id","title","completed"])
+        for todo in todos :
+            if todo['userId']==user['id'] :
+                    writer.writerow(list(todo.values())[1:])
+
+
+        
+    
+  
